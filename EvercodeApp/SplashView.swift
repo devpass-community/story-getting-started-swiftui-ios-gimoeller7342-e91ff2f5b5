68dc47c877a95c530b07bd2714eec736d471c0a5
giovannamoeller <giovannagmoeller@gmail.com>
2023-05-15 15:40:48 -0300

    splash view

--- EvercodeApp/SplashView.swift
+++ EvercodeApp/SplashView.swift
@@ -2,7 +2,24 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text("Welcome to Evercode!")
+      VStack {
+        Image("EvercodeLogo")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 100, height: 100)
+        
+        if #available(iOS 16.0, *) {
+          Text("Welcome to Evercode!")
+            .padding(16)
+            .font(.title)
+            .fontWeight(.semibold)
+        } else {
+          Text("Welcome to Evercode!")
+            .padding(16)
+            .font(.title)
+        }
+        ProgressView()
+      }
     }
 }
 
